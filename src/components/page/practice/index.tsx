d20907c14c006ff45470db0d8118ac226ed8cace
Diep Dev <diepkim1919@gmail.com>
2025-03-28 03:19:46 +0700

    commit refector code

--- src/components/page/practice/index.tsx
+++ src/components/page/practice/index.tsx
@@ -4,14 +4,11 @@ import {
   ChatBubbleBottomCenterIcon,
   CheckCircleIcon,
   MicrophoneIcon,
-  SpeakerWaveIcon,
 } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
+import Example from "./components/example";
+import Pronuciation from "./components/pronuciation";
 
 function Practice() {
   const tabs = [
@@ -66,12 +63,9 @@ function Practice() {
               onChange={(e) => setActiveTab(e.target.value)}
             >
               {tabs.map((tab) => (
-                <div className="flex items-center">
-                  {tab.icon}
-                  <option key={tab.code} value={tab.code}>
-                    {tab.name}
-                  </option>
-                </div>
+                <option key={tab.code} value={tab.code}>
+                  {tab.name}
+                </option>
               ))}
             </select>
           </div>
@@ -95,57 +89,8 @@ function Practice() {
           </div>
         </div>
       </div>
-      <div>
-        <div className="max-w-screen-lg p-4 mx-auto bg-[#190b37] border border-[#291650] rounded-[16px] shadow-sm sm:p-8 ">
-          <h5 className="mb-2 text-xl font-[600] text-white">
-            Pronunciation Practice
-          </h5>
-          <p className="mb-5 text-base text-white text-[14px]">
-            Practice your pronunciation with our Al feedback system. Speak
-            clearly into your microphone, and our Al will analyze your speech
-            patterns to provide you with detailed feedback.
-          </p>
-
-          <div className="border bg-[#201041] border-[#291650] mb-4 rounded-[16px] p-4">
-            <h5 className="mb-2 text-sm text-white">Your exercise:</h5>
-            <p className="mb-5 text-white text-lg italic">
-              "How much wood would a woodchuck chuck if a woodchuck could chuck
-              wood?"
-            </p>
-          </div>
-          <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 cursor-pointer">
-            <a className="w-full sm:w-auto bg-[#8861ea] focus:ring-4 focus:outline-none text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 ">
-              <div className="text-left flex items-center gap-2">
-                <MicrophoneIcon className="w-4 h-4" />
-                <div className="text-xs">Start Recording</div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="mt-10">
-        <div className="max-w-screen-lg p-4 mx-auto bg-[#190b37] border border-[#291650] rounded-[16px] shadow-sm sm:p-8 ">
-          <div className="flex items-center gap-4">
-            <SpeakerWaveIcon className="w-6 h-6 text-[#7c57d7]" />
-            <h5 className="mb-2 text-xl font-[600] text-white">
-              Example Pronunciation
-            </h5>
-          </div>
-          <p className="mb-5 text-base text-gray-500 sm:text-lg ">
-            Listen to a native speaker pronounce the exercise phrase to help you
-            with your practice.
-          </p>
-
-          <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 cursor-pointer">
-            <a className="w-full sm:w-auto bg-black border border-[#291650] focus:ring-4 focus:outline-none text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 ">
-              <div className="text-left flex items-center gap-2">
-                <SpeakerWaveIcon className="w-4 h-4 " />
-                <div className="text-xs">Play Audio Sample</div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
+      <Pronuciation />
+      <Example />
     </>
   );
 }
